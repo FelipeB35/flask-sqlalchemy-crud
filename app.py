94bfda3d2d9643b87231860de06217b0b1c03663
Felipe B <20200754@esen.edu.sv>
2022-03-19 13:59:46 -0600

    flask Blueprint

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from flask import Flask
-
+from routes.contacts import contacts
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello world"
-
-if "__main__" == __name__:
-  app.run(debug=True)
-
+app.register_blueprint(contacts)
